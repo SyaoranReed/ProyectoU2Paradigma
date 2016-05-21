ab6a81e717a544ba2a70fa4aaef9ca7a7db06e19
Syaoran <Syaoran@Stocke>
2016-05-21 13:31:35 -0300

    probando bst

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,14 +17,18 @@ public class Main
     public static void main(String[] args)
     {
         BST<Integer> bst = new BST<Integer>();
-        bst.insertar(4);
-        bst.insertar(7);
-        bst.insertar(1);
-        bst.insertar(8);
-        bst.insertar(3);
-        bst.insertar(12);
-        System.out.println(bst.toString()); //Salida: 1 3 4 7 8 12
-        System.out.println(bst.buscar(7)); //true
-        System.out.println(bst.buscar(5)); //false
+        Random random = new Random(System.currentTimeMillis());
+        int i = 0;
+        while (i++ < 100){
+            int r = random.nextInt(1000);
+            
+            bst.insertar(r);
+            
+            if (r > 500){
+                System.out.printf("%d : %b\n",r,bst.buscar(r));
+            }
+        }
+        
+        System.out.println(bst.toString());
     }
 }
